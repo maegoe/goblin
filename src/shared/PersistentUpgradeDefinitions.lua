21223049bfecd92f253f0174249dda314f5763c3
S-Hun <qotjrgnsg@gmail.com>
2026-06-28 10:03:37 +0900

    feat: batch V1 follow-up Jira work

--- src/shared/PersistentUpgradeDefinitions.lua
+++ src/shared/PersistentUpgradeDefinitions.lua
@@ -1,19 +1,22 @@
+local MAX_HEALTH_COSTS = { 10, 15, 25, 40, 60, 85, 115, 150, 190, 235 }
+local ATTACK_DAMAGE_COSTS = { 10, 15, 25, 40, 60, 85, 115, 150, 190, 235 }
+
 local PersistentUpgradeDefinitions = {
 	MaxHealth = {
 		Id = "MaxHealth",
 		DisplayNameKey = "persistent.maxHealth",
 		StatKey = "MaxHealth",
 		ValuePerLevel = 5,
-		MaxLevel = 5,
-		Costs = { 10, 15, 25, 40, 60 },
+		MaxLevel = #MAX_HEALTH_COSTS,
+		Costs = MAX_HEALTH_COSTS,
 	},
 	AttackDamage = {
 		Id = "AttackDamage",
 		DisplayNameKey = "persistent.attackDamage",
 		StatKey = "AttackDamage",
 		ValuePerLevel = 1,
-		MaxLevel = 5,
-		Costs = { 10, 15, 25, 40, 60 },
+		MaxLevel = #ATTACK_DAMAGE_COSTS,
+		Costs = ATTACK_DAMAGE_COSTS,
 	},
 }
 
